3e62c6d451ec93e6e52e0f244bec0043e4d34ae2
Raveena Sharma <raveenasharma@raveenas-mbp.home>
2019-12-11 10:12:56 +0000

    First commit

--- src/NewsCard.js
+++ src/NewsCard.js
@@ -24,7 +24,7 @@ export const NewsCard = ({ cardId, title, author, text, score }) => {
                     </div>
 
                 </div>
-                {/* Dummy preview text as the HN News API does not provide one */}
+                {/* Dummy preview text as the HN News API does not have the attribute for every story */}
                 <div className="card-content">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                 </div>
